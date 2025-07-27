49bd538f1c262e79276ff901503a2b653cb8bec2
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-07-27 19:49:47 +0300

    feat: Создание события. Подготовка формы.

---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { CreateEventForm } from '@/features/create-event'
+
+export default function CreateEvent() {
+  return (
+    <div className="mx-auto max-w-4xl">
+      <CreateEventForm />
+    </div>
+  )
+}
