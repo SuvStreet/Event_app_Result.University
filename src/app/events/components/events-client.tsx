9d83f84a2e9e4577e22701576779c352ee40c5d2
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-07-28 17:15:10 +0300

    refactor: Переименовать клиентские компоненты от серверных.

---
+++ src/app/events/components/events-client.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { CreateEventForm } from '@/features/create-event'
+
+export function EventsClient() {
+  return (
+    <div className="mx-auto max-w-4xl">
+      <CreateEventForm />
+    </div>
+  )
+}
