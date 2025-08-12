c7a8c8cc41fd605747e69a3f9aa9776ca1f0135f
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-08-12 22:08:58 +0300

    feat: Защита страницы от неавторизованных пользователей.

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,5 +1,11 @@
 import { EventDetailLayout } from '@/features/event-detail'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 export default async function Event() {
+  const session = await getServerSession()
+
+  if (!session) redirect('/api/auth/signin')
+
   return <EventDetailLayout />
 }
