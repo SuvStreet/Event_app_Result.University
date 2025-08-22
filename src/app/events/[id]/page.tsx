fd28b3e1d44799181e25c6fe0d121a40a0801c6d
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-08-22 19:13:23 +0300

    feat: Убрать кнопку действия не вошедших пользователей.

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,11 +1,5 @@
 import { EventDetailLayout } from '@/features/event-detail'
-import { getServerSession } from 'next-auth'
-import { redirect } from 'next/navigation'
 
 export default async function Event() {
-  const session = await getServerSession()
-
-  if (!session) redirect('/api/auth/signin')
-
   return <EventDetailLayout />
 }
